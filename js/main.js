769d937f7b65dc6dec0993e12fd3ec36448a73d2
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-03-09 22:13:04 +0900

    [Feat] 이미지 순차적으로 보이게 애니메이션 구현

--- js/main.js
+++ js/main.js
@@ -33,3 +33,12 @@ window.addEventListener(
     }
   }, 300)
 );
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.6,
+    opacity: 1,
+  });
+});

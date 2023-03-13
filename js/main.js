9c6685c5ba243ed5c131f095d1446e57bfbbede4
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-03-13 18:10:15 +0900

    [Feat] 유튜브 영상 배경 구현, 반복 애니메이션 구현

--- js/main.js
+++ js/main.js
@@ -82,3 +82,21 @@ promotionToggleBtn.addEventListener("click", function () {
     promotionEl.classList.remove("hide");
   }
 });
+
+function random(min, max) {
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay),
+  });
+}
+
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", 0.5, 15);
+floatingObject(".floating3", 1.5, 20);
